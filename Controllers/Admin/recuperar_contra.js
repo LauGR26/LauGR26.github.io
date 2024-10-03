b2aa2b1d73805434822985ffe0c5a1bf14df4104
LauGR26 <163802219+LauGR26@users.noreply.github.com>
2024-10-02 20:08:53 -0500

    Add files via upload

---
+++ Controllers/Admin/recuperar_contra.js
@@ -0,0 +1,31 @@
+import { userstate, verificationcod}from "../../Controllers/conection.js";
+
+userstate()
+
+const enviarCorreo = document.getElementById('btnRecuperar')
+const atrasSesion = document.getElementById('btnatras')
+
+async function recuperarContraseña() {
+    const email = document.getElementById('edtemail').value;
+
+    try {
+        const validation = await verificationcod(email);
+        alert("Reset verification successful: " + email);
+        // Redirecciona al administrador
+        window.location.href = "/Templates/Admin/main_admin.html";
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(`Error ${errorCode}: ${errorMessage}`);
+    }
+}
+
+async function atras() {
+    window.location.href = "/Templates/Admin/main_admin.html"
+    
+}
+
+window.addEventListener('DOMContentLoaded',async()=>{
+    enviarCorreo.addEventListener('click',recuperarContraseña)
+    atrasSesion.addEventListener('click',atras)
+})
